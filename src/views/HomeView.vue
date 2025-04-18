c6ad7929768b0d10905faf8ad3faa5426bb25f2e
sangil6372 <sangil6372@naver.com>
2025-04-18 16:46:02 +0900

    init : Vue 초기 세팅

---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Home 페이지</h1>
+</template>
+
+<script setup></script>
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    proxy: {
+      // '/api'로 시작하는 요청은 http://localhost:3000으로 보냄
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '') // '/api' 접두사 제거
+      }
+    }
+  }
+})
